7701b6b4dd9d7833b670ba1179850d37eabdda02
Shirley B. Aguilar <shirley.aguilar.agui@gmail.com>
2022-05-25 20:00:31 -0500

    Testing al 90%

---
+++ examples/folder/directory1/readmeHostNameError7.md
@@ -0,0 +1,3 @@
+Hola, bienvenido a un texto sin links
+[`link con hostname inexistente`](https://fakecccaa1.com)
+[`link con hostname inexistente`](ftp://fakecccaa1.com)
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,44 +1,43 @@
 // jest.mock('../utils.js')
 const {
     mdLinks,
-    validateRoute,
     getExistOption,
     cliFunction,
   } = require("../src/index");
 
-  const {
-    getPropertiesOfObject,
-  } = require("../src/methods");
-  const {
-    getStatsUniqueBroken,
-  } = require("../src/utils");
+const {
+  getPropertiesOfObject,
+} = require("../src/methods");
+const {
+  getStatsUniqueBroken,
+} = require("../src/utils");
 
-  jest.mock("chalk", () => ({
-    green: jest.fn(( ) => "green") ,
-    yellow:jest.fn(( ) => "yellow"),
-    red: jest.fn(( ) => "red"),
-    yellowBright: jest.fn(( ) => "yellowBright"),
-    blue: jest.fn(( ) => "blue"),
-    bgRed: jest.fn(( ) => "bgRed"),
-    bold: {
-      redBright: jest.fn(( ) => "redBright") ,
-      magenta:jest.fn(( ) => "magenta") ,
-  }
-  }));
+jest.mock("chalk", () => ({
+  green: jest.fn(( ) => "green") ,
+  yellow:jest.fn(( ) => "yellow"),
+  red: jest.fn(( ) => "red"),
+  yellowBright: jest.fn(( ) => "yellowBright"),
+  blue: jest.fn(( ) => "blue"),
+  bgRed: jest.fn(( ) => "bgRed"),
+  bold: {
+    redBright: jest.fn(( ) => "redBright") ,
+    magenta:jest.fn(( ) => "magenta") ,
+}
+}));
 describe("mdLinks", () => {
-  it("G", () => {
+  it("Return a array object (href,text,file,statusCode, status)", () => {
     const route = "./examples/folder/directory1/readme6.md"
     const option = { validate : true};
     const result = [
       {
-        file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
+        file: 'C:\\Users\\ruben\\Desktop\\md-link\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
         href: 'https://docs.npmjs.com/cli/install/shirley',
         text: 'docs oficiales de npm install (más de 50 caractere',
         statusCode: 404,
         status: 'Fail'
       },
       {
-        file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
+        file: 'C:\\Users\\ruben\\Desktop\\md-link\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
         href: 'https://github.com/Laboratoria/course-parser',
         text: '`course-parser`',
         statusCode: 301,
@@ -49,17 +48,17 @@ describe("mdLinks", () => {
    expect(arrayObject).toEqual(result)
    )
   })
-  it("G", () => {
+  it("Return a array object (href,text,file)", () => {
     const route = "./examples/folder/directory1/readme6.md"
     const option = { validate : false}
     const result = [
       {
-        file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
+        file: 'C:\\Users\\ruben\\Desktop\\md-link\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
         href: 'https://docs.npmjs.com/cli/install/shirley',
         text: 'docs oficiales de npm install (más de 50 caractere',
       },
       {
-        file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
+        file: 'C:\\Users\\ruben\\Desktop\\md-link\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
         href: 'https://github.com/Laboratoria/course-parser',
         text: '`course-parser`',
       }
@@ -72,37 +71,37 @@ describe("mdLinks", () => {
 });
 
 describe("cliFunction", () => {
-  it("return a message if the option is not valid", () => {
+  it("Return a message if the option is not valid", () => {
     const route = "./examples/folder/directory1/readme6.md"
     const option = ["--sta", undefined]
     expect(cliFunction(route, option)).toBe("Sorry, this option does not exist.");
   });
-  it("return a message if the route is not valid", () => {
+  it("Return a message if the route is not valid", () => {
     const route = "./examples/folder/directory1/readme"
     const option = ["--stats", undefined]
     expect(cliFunction(route, option)).toBe("Sorry, this route does not exist.");
   });
-  it("oh2", () => {
+  it("Return route processed if stats were displayed", () => {
     const route = "./examples/folder/directory1/readme6.md";
     const option = [undefined];
     expect(cliFunction(route, option)).toBe("route processed");
   })
-  it("oh2", () => {
+  it("Return route processed if stats were displayed", () => {
     const route = "./examples/folder/directory1/readme6.md";
     const option = ["--stats", undefined];
     expect(cliFunction(route, option)).toBe("route processed");
   })
-  it("oh2", () => {
+  it("Return route processed if stats were displayed", () => {
     const route = "./examples/folder/directory1/readme6.md";
     const option = ["--validate", undefined];
     expect(cliFunction(route, option)).toBe("route processed");
   })
-  it("oh2", () => {
+  it("Return route processed if stats were displayed", () => {
     const route = "./examples/folder/directory1/readme6.md";
     const option = ["--validate", "--stats"];
     expect(cliFunction(route, option)).toBe("route processed");
   })
-  it("oh2", () => {
+  it("Return route processed if stats were displayed", () => {
     const route = "./examples/folder/directory1/readme6.md";
     const option = ["--validate", undefined];
     expect(cliFunction(route, option)).toBe("route processed");
@@ -110,19 +109,19 @@ describe("cliFunction", () => {
 })
 
 describe("getExistOption", () => {
-    it("Return a booleano true if comand exist", () => {
+    it("Return true if comand exist", () => {
         const input = ["--validate", undefined];
           expect(getExistOption(input)).toBe(true);
       });
-      it("Return a booleano true if comand exist", () => {
+      it("Return true if comand exist", () => {
         const input = ["--validate", "--stats" ];
           expect(getExistOption(input)).toBe(true);
       })
-      it("Return a booleano false if comand exist", () => {
+      it("Return false if comand exist", () => {
         const input = ["--stats", undefined];
           expect(getExistOption(input)).toBe(false);
       });
-      it("Return a booleano false if comand exist", () => {
+      it("Return false if comand exist", () => {
         const input = [undefined];
           expect(getExistOption(input)).toBe(false);
       });
@@ -135,78 +134,3 @@ describe("getExistOption", () => {
           expect(getExistOption(input)).toBe("inexistente");
       });
   })
-
-/* describe("mdLinks", () => {
-    it("fff", () => {
-        const route = "./examples/folder/directory1/readme6.md";
-        const option = true;
-        const input = ["--validate", undefined]
-    expect( mdLinks(route, option, input)).resolves.toEqual([
-        {
-          file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
-          href: 'https://docs.npmjs.com/cli/install/shirley',
-          text: 'docs oficiales de `npm install` acá más de 50 line',
-          statusCode: 404,
-          status: 'Fail'
-        },
-        {
-          file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
-          href: 'https://github.com/Laboratoria/course-parser',
-          text: '`course-parser`',
-          statusCode: 301,
-          status: 'Ok'
-        }
-      ]);
-    })
-    it("fffvvv", () => {
-        const route = "./examples/folder/directory1/readme6.md";
-        const option = true;
-        const input = ["--validate", "--stats" ]
-    expect( mdLinks(route, option, input)).resolves.toEqual([
-        {
-          file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
-          href: 'https://docs.npmjs.com/cli/install/shirley',
-          text: 'docs oficiales de `npm install` acá más de 50 line',
-          statusCode: 404,
-          status: 'Fail'
-        },
-        {
-          file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
-          href: 'https://github.com/Laboratoria/course-parser',
-          text: '`course-parser`',
-          statusCode: 301,
-          status: 'Ok'
-        }
-      ]);
-    })
-    it("fffvvv", () => {
-        const route = "./examples/folder/directory1/readme6.md";
-        const option = true;
-        const input = ["--validate", "--stats" ]
-    expect( mdLinks(route, option, input)).resolves.toEqual(
-    [
-        {
-            file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
-            href: 'https://docs.npmjs.com/cli/install/shirley',
-            text: 'docs oficiales de `npm install` acá más de 50 line',
-            statusCode: 404,
-            status: 'Fail'
-          },
-          {
-            file: 'C:\\Users\\ruben\\Desktop\\MD-LINKS\\LIM017-md-links\\examples\\folder\\directory1\\readme6.md',
-            href: 'https://github.com/Laboratoria/course-parser',
-            text: '`course-parser`',
-            statusCode: 301,
-            status: 'Ok'
-          }
-    ]  
-    )});
-
-    it("fffvvv", () => {
-        const route = "./examples/folder/directory1/readme6";
-        const option = true;
-        const input = ["--validate", "--stats" ]
-    expect( mdLinks(route, option, input)).reject.toEqual(console.error("Sorry, this route does not exist."));
-});
-});
- */
